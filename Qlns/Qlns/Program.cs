02d59ccae3c7d7e764118ed7f892bc02de18fc3e
Hai-Anh270203 <117918022+Hai-Anh270203@users.noreply.github.com>
2023-12-10 23:17:28 +0700

    Quản lý nhân sự
    File giao diện

---
+++ Qlns/Qlns/Program.cs
@@ -0,0 +1,24 @@
+﻿using Q;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WinFormsApp1;
+
+namespace Qlns
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new dangnhap());
+        }
+    }
+}
